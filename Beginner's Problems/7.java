75a96d258857d9634e1bc588a3fdc95e5a18c89f
abhinivesh3 <112188751+abhinivesh3@users.noreply.github.com>
2022-10-12 14:44:46 +0530

    commit

--- Beginner's Problems/7.java
+++ Beginner's Problems/7.java
@@ -1,5 +1,6 @@
 import java.util.*;
-public class  Main{
+
+public class Main {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in){
             int N=scanner.nextInt();
