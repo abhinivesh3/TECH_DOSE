f49239a420f0c92b2c21b3a1736aaf3d46de44a0
abhinivesh3 <112188751+abhinivesh3@users.noreply.github.com>
2022-10-19 14:58:11 +0530

    commit

--- LC-WeeklyContent/LC-265-2057.SmallestIndexWithEqualValue.java
+++ LC-WeeklyContent/LC-265-2057.SmallestIndexWithEqualValue.java
@@ -1,13 +1,13 @@
-class Solution {
-    public int smallestEqual(int[] nums) {
-        int n=nums.length;
-        for(int i=0;i<n;i+=1){
-            if(i%10==nums[i]){
-                return i;
+public class LC-265-2057.SmallestIndexWithEqualValue {
+    class Solution {
+        public int smallestEqual(int[] nums) {
+            int n=nums.length;
+            for(int i=0;i<n;i+=1){
+                if(i%10==nums[i]){
+                    return i;
+                }
             }
+            return -1;
         }
-        return -1;
     }
-}public class LC-265-2057.SmallestIndexWithEqualValue {
-    
 }
