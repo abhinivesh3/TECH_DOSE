3b146f640ab7df3a40b95093dbeaede24dad64bf
abhinivesh3 <112188751+abhinivesh3@users.noreply.github.com>
2022-10-20 12:39:38 +0530

    commit

---
+++ LC-WeeklyContent/LC-2-389.FindtheDifference.java
@@ -0,0 +1,14 @@
+public class LC-2-389.FindtheDifference {
+    class Solution {
+        public char findTheDifference(String s, String t) {
+            char ch = 0;
+            for(char cs : s.toCharArray()){
+                ch ^= cs;
+            }
+            for(char ct : t.toCharArray()){
+                ch ^= ct;
+            }
+            return ch;
+        }
+    }
+}
