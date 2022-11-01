4ad8706c3c845a391621458e9b449200fa846eeb
abhinivesh3 <112188751+abhinivesh3@users.noreply.github.com>
2022-11-01 15:03:07 +0530

    commit

---
+++ Chapters/LinkedList/430. Flatten a Multilevel Doubly Linked List.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class 430. Flatten a Multilevel Doubly Linked List {
+    class Solution {
+        public Node flatten(Node head) {
+            if(head==null){
+                return head;
+            }
+            Node curr = head;
+            while(curr!=null){
+                if(curr.child!=null){
+                    Node tail = curr.child;
+                    while(tail.next!=null){
+                        tail=tail.next;
+                    }
+                    tail.next=curr.next;
+                    if(curr.next!=null){
+                        curr.next.prev=tail;
+                    }
+                    curr.next=curr.child;
+                    curr.child.prev=curr;
+                    curr.child=null;
+                }
+                curr=curr.next;
+            }
+            return head;
+        }
+    }
+}
